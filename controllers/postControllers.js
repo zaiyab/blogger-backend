320267146416e37a10969c9621b5f2bb61f74904
zaiyab <zaiyabdati@gmail.com>
2023-08-27 21:51:33 +0500

    working

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -7,8 +7,8 @@ import { v4 as uuidv4 } from "uuid";
 const createPost = async (req, res, next) => {
   try {
     const post = new Post({
-      title: "sample title",
-      caption: "sample caption",
+      title: req.body.title,
+      caption: req.body.desc,
       slug: uuidv4(),
       body: {
         type: "doc",
